fa2c61e90df083876d4b7d2054ced503b773d836
Matheus <matheuspsilva29@gmail.com>
2022-05-09 16:49:44 -0300

    Primeiro Commit

---
+++ src/test/java/com/matheussilvadev/SpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheussilvadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
