44d612136f42a0532ee577d7eca1b4eb9e621070
Matheus <matheuspsilva29@gmail.com>
2022-05-10 14:33:17 -0300

    Criando nosso serviço POST e efetuando um cadastro de usuário

--- src/main/java/com/matheussilvadev/controller/IndexController.java
+++ src/main/java/com/matheussilvadev/controller/IndexController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +18,23 @@ import com.matheussilvadev.model.Usuario;
 import com.matheussilvadev.repository.UsuarioRepository;
 
 @RestController
-@RequestMapping(value = "/usuario")
+@RequestMapping(value = "/usuarios")
 public class IndexController {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@GetMapping(value = "/{id}/relatorio-pdf", produces = "application/json")
+	public ResponseEntity<Usuario> relatorioUsuario(@PathVariable(value = "id") Long id) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+		
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
 	@GetMapping(value = "/{id}", produces = "application/json")
-	public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) {
+	public ResponseEntity<Usuario> usuario(@PathVariable(value = "id") Long id) {
 		
 		Optional<Usuario> usuario = usuarioRepository.findById(id);
 
@@ -39,7 +50,13 @@ public class IndexController {
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
 	
-	
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
 	
 
 }
