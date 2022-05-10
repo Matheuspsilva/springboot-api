b3ef39e29913014794e6250ea48ce9aa8ba4ffd6
Matheus <matheuspsilva29@gmail.com>
2022-05-10 16:09:32 -0300

    Evitando recursividade e gerando o JSON de pai e filhos

--- src/main/java/com/matheussilvadev/model/Usuario.java
+++ src/main/java/com/matheussilvadev/model/Usuario.java
@@ -65,6 +65,14 @@ public class Usuario implements Serializable {
 		this.nome = nome;
 	}
 
+	public List<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
