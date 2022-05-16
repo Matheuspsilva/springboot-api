4f3c0af1b870f8f9e7e320eccb92386dd1397e13
Matheus <matheuspsilva29@gmail.com>
2022-05-16 14:09:41 -0300

    Configurando o Repository e o Service Spring Security

--- src/main/java/com/matheussilvadev/repository/UsuarioRepository.java
+++ src/main/java/com/matheussilvadev/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.matheussilvadev.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,8 @@ import com.matheussilvadev.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+
+	@Query("select u from Usuario u where u.login = ?1")
+	Usuario findUserByLogin(String id);
 
 }
