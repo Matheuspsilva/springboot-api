656ce1696f6060a620a45b5e58fc1d7a8e675456
Matheus <matheuspsilva29@gmail.com>
2022-05-17 11:29:31 -0300

    Criando a classe JwtLoginFilter

--- src/main/java/com/matheussilvadev/security/JWTTokenAutenticacaoService.java
+++ src/main/java/com/matheussilvadev/security/JWTTokenAutenticacaoService.java
@@ -1,5 +1,6 @@
 package com.matheussilvadev.security;
 
+import java.io.IOException;
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,7 +35,7 @@ public class JWTTokenAutenticacaoService {
 	private static final String HEADER_STRING = "Authorization";
 
 	// Gerando token de autenticação e adicionando ao cabeçalho e resposta Http
-	public void addAuthentication(HttpServletResponse response, String username) throws Exception {
+	public void addAuthentication(HttpServletResponse response, String username) throws IOException {
 		// Montagem do Token
 		String JWT = Jwts.builder()// Chama o gerador de token
 				.setSubject(username) // Adiciona o usuário
